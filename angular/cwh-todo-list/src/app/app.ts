cbcb28d5298b2b4448830f016d0ad4d08cc60f86
razashoeb840 <razashoeb3051@gmail.com>
2026-04-10 13:59:48 +0530

    Add files

--- angular/cwh-todo-list/src/app/app.ts
+++ angular/cwh-todo-list/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('infosys');
+  protected readonly title = signal('cwh-todo-list');
 }
--- infosys/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Login } from './login/login';
-import { Register } from './register/register';
-
-export const routes: Routes = [
-  { path: '', component: Login },
-  { path: 'register', component: Register }
-];
